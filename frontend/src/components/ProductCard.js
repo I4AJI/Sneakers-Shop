3beee104f4dceb4fcf0fa4915e793542aec16e07
I4AJI <151009098+I4AJI@users.noreply.github.com>
2025-03-14 03:13:54 +0200

    Update ProductCard.js

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import { motion } from 'framer-motion';
